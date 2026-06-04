641bcfeac05e07aed68ad2e5a6ff4c2a9bb69f40
artem <artemnurmetow@gmail.com>
2026-06-04 16:58:24 +0200

    changes server.js

--- server.js
+++ server.js
@@ -243,6 +243,9 @@ app.get('/admin', basicAuth, (req, res) => {
 });
 
 // Start server
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
